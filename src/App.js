29aa7595dabb32502d5218b9a5e8f3491a0b9b10
Keaton Sentak <k.sentak@outlook.com>
2020-12-01 20:39:54 -0500

    setup functions

--- src/App.js
+++ src/App.js
@@ -4,12 +4,30 @@ import jsonData from './restaurants.json';
 import './App.css';
 
 const App = () => {
-	const [foodData, setFoodData] = useState({});
+	const [foodData, setFoodData] = useState([]);
+	const [randomArr, setRandomArr] = useState([]);
+	const [chickenDinner, setChickenDinner] = useState('');
 
+	// Loads in json restaurant data
 	const loadData = () => {
 		const restaurantJson = JSON.parse(JSON.stringify(jsonData));
 		// console.log(restaurantJson.restaurants);
 		setFoodData(restaurantJson.restaurants);
+	};
+
+	// Adds selected restaurants to list
+	const printRandomArr = (arr) => {
+		for (let i = 0; i < arr.length; i++) {
+			return arr[i] + ', ';
+		}
+	};
+
+	// Selects dinner
+	const selectDinner = () => {
+		const winnerWinner =
+			randomArr[Math.floor(Math.random() * randomArr.length)];
+		console.log(winnerWinner.toString());
+		// setChickenDinner(winnerWinner);
 	};
 
 	useEffect(() => {
@@ -24,19 +42,31 @@ const App = () => {
 					Select potential food options and this app will automatically decide
 					what you should have for dinner
 				</p>
-				<button className='btn btn-primary'>Randomize</button>
+				<button className='btn btn-primary' onClick={selectDinner}>
+					Randomize
+				</button>
+				<p>{chickenDinner}</p>
+			</div>
+
+			<div className='selected-list'>
+				<h6>Your food options to be selected from:</h6>
+				{/* <p>{printRandomArr(randomArr)}</p> */}
 			</div>
 			<div className='card-container'>
-				{foodData.map((item) => {
-					return (
-						<Card
-							key={item.id}
-							restaurant={item.name}
-							city={item.city}
-							cuisine={item.cuisine}
-						/>
-					);
-				})}
+				<div className='row'>
+					{foodData.map((item) => {
+						return (
+							<div className='col-sm-3'>
+								<Card
+									key={item.id}
+									restaurant={item.name}
+									city={item.city}
+									cuisine={item.cuisine}
+								/>
+							</div>
+						);
+					})}
+				</div>
 			</div>
 		</div>
 	);
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,30 @@
-// import { useEffect } from 'react';
+import { useState } from 'react';
+import './Card.css';
 
 const Card = (props) => {
+	const [isSelected, changeIsSelected] = useState(false);
+
+	const selectRestaurant = () => {
+		changeIsSelected(!isSelected);
+	};
+
 	return (
 		<div className='card-component'>
-			<div className='card col-md-3'>
+			<div className='card'>
 				<div className='card-body'>
 					<h3 className='card-title'>{props.restaurant}</h3>
 					<div className='card-text'>
 						<p className='city mb-0'>City: {props.city}</p>
 						<p className='cuisine'>Cuisine: {props.cuisine}</p>
-						<button className='btn btn-primary'>Add to list</button>
+						{isSelected ? (
+							<button className='btn btn-danger' onClick={selectRestaurant}>
+								Remove from list
+							</button>
+						) : (
+							<button className='btn btn-primary' onClick={selectRestaurant}>
+								Add to list
+							</button>
+						)}
 					</div>
 				</div>
 			</div>
